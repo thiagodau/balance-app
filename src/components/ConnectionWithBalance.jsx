3035e18ed6289ae8b81d17f351f58b0f2d58cb32
Thiago <thiagorodriguesdau@gmail.com>
2024-01-10 15:02:39 -0400

    Adicionado função de conectar e desconectar Serial

--- src/components/ConnectionWithBalance.jsx
+++ src/components/ConnectionWithBalance.jsx
@@ -1,39 +1,58 @@
-import Button from './Button'
+import { useState } from "react";
+import Button from "./Button";
 
 export default function ConnectionWithBalance({ baudRate }) {
   let baudRateCurrent = baudRate;
-  
-  //função para conectar uma serial
+
+
+  const [statusBalance, setStatusBalance] = useState(false);
+  const [device, setDevice] = useState()
+
+  //Function to Connect on Serial
   async function connectSerial() {
     try {
-      console.log("Selecione uma serial...");
       let device = await navigator.serial.requestPort();
       await device.open({ baudRate: baudRateCurrent });
-      console.log(device);
-      console.log("Dispositivo conectado!");
-      //readToSerial(device);
+      if (device) {
+        setStatusBalance(true);
+        setDevice(device)
+        console.log("Dispositivo conectado!");
+        console.log(device.getInfo());
+        //readToSerial(device);
+      }
       return;
     } catch (error) {
-      console.log(
-        "Nenhuma serial selecionada ou algum erro aconteceu. Erro: " + error
-      );
+      console.log(error);
     }
   }
 
+  //Function to Disconnect on Serial
+  async function disconnectSerial(device) {
+    await device.close();
+    setStatusBalance(false);
+    console.log("Dispositivo desconectado!");
+  }
+
   async function readToSerial(device) {
-    //read data of serial port
     const reader = device.readable.getReader();
-    
     while(true) {
       const { value, done } = await reader.read();
       console.log(value);
+      if(done) {
+        //aqui para de ler
+        break;
+      }
     }
   }
 
   return (
     <>
-      <h3>Conexão com a Balança</h3>
-      <Button titleOfButton={'Conectar a Balança'} func={connectSerial}/>
+      <h3>
+        Conexão com a Balança - Status:{" "}
+        {statusBalance ? "Conectada!" : "Desconectada!"}
+      </h3>
+      <Button titleOfButton={"Conectar a Balança"} func={connectSerial} />
+      <button onClick={() => disconnectSerial(device)}>Desconectar</button>
     </>
   );
 }
